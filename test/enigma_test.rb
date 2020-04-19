11fd2ecbb5380bbbe1d2555d41938bbc69a760b7
Jane Greene <janegreenecasa@gmail.com>
2020-04-19 15:15:47 -0600

    removed interation comments that are covered

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -150,24 +150,7 @@ class EnigmaTest < Minitest::Test
 
 end
 
-#
-# # encrypt a message with a key and date
-# pry(main)> enigma.encrypt("hello world", "02715", "040895")
-# #=>
-# #   {
-# #     encryption: "keder ohulw",
-# #     key: "02715",
-# #     date: "040895"
-# #   }
-#
-# # decrypt a message with a key and date
-# pry(main) > enigma.decrypt("keder ohulw", "02715", "040895")
-# #=>
-# #   {
-# #     decryption: "hello world",
-# #     key: "02715",
-# #     date: "040895"
-# #   }
+
 #
 # # encrypt a message with a key (uses today's date)
 # pry(main)> encrypted = enigma.encrypt("hello world", "02715")
