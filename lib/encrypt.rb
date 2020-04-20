5c491e9dcbe4aa43e534c5359dda46407c08955b
Jane Greene <janegreenecasa@gmail.com>
2020-04-19 18:04:51 -0600

    encrypt runner working

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,1 +1,15 @@
-require './lib/enigma'
+require_relative './enigma'
+
+handle = File.open(ARGV[0],"r")
+
+incoming_file = handle.read
+
+handle.close
+enigma1 = Enigma.new
+encrypted_text = enigma1.encrypt(incoming_file)
+
+writer = File.open(ARGV[1],"w")
+writer.write(encrypted_text[:encryption]) #need to pull out encrypted test and key
+writer.close
+
+puts "Created '" + ARGV[1] + "' with the key " + enigma1.key + " and date " + enigma1.date
